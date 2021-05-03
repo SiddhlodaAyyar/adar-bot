d75e7e7b93b12063939bb9af7f8c4a4e12ddd9ba
Yash G <51716673+SiddhlodaAyyar@users.noreply.github.com>
2021-05-03 19:13:37 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -35,7 +35,7 @@ while 1:
                 avl = y["available_capacity"]
                 age_limit = y["min_age_limit"]
                 log_time = datetime.now().strftime("%H:%M:%S")
-                if (avl > 0) and (age_limit == 45):
+                if (avl > 0) and (age_limit == 18):
                     data = {
                         'chat_id': '-599964487',
                         'text': 'Vaccine aya hai bsdk, book karle.\n\n '+'Avl_Date : '+date+'\n\nVaccine Capacity : '+str(avl)+'\n\n' + center_name + ' : idhar jaake marwa ke le.\n\n' + str(pincode) + ' : iss pincode pe.\n\n'+'age_limit : ' + str(age_limit) + '. 18 saal ka hoga toh jaa nahi toh gharmei baithke porn dekh.\n\n'+'Last Checked availability : ' + log_time

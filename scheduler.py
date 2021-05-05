448750ba6e41caf002397d791ebab8278e23b017
Yash G <51716673+SiddhlodaAyyar@users.noreply.github.com>
2021-05-05 23:06:00 +0530

    Update scheduler.py

--- scheduler.py
+++ scheduler.py
@@ -9,25 +9,26 @@ import sys
 
 from loguru import logger
 
+
 def sink(message):
-  record = message.record
-  if record.get("name") == "your_specific_logger":
-    print("Log comes from your specific logger")
+    record = message.record
+    if record.get("name") == "your_specific_logger":
+        print("Log comes from your specific logger")
+
 
 logger = logger.bind(name="your_specific_logger")
 
 
-
-
 def getAvl(pincode, date):
     # req = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=" + pincode + "&date=" + date
-    try:
-        req = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=" + pincode + "&date=" + date
-        headers = {
+
+    req = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=" + pincode + "&date=" + date
+    headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-        
-        li = requests.get(req, headers=headers).json()["centers"]
-        li.raise_for_status()
+    try:
+        data = requests.get(req, headers=headers)
+        li = data.json()["centers"]
+        data.raise_for_status()
         return li
     except requests.exceptions.HTTPError as errh:
         logger.info(errh)
@@ -47,9 +48,9 @@ def getAvl(pincode, date):
         logger.info(message)
         return []
 
-        
 
-pincodes = ["411011", "411001", "411033", "411006", "411028", "411038","411041","411027","411018","411035","411026","411017","411044"]
+pincodes = ["411011", "411001", "411033", "411006", "411028", "411038", "411041", "411027", "411018", "411035",
+            "411026", "411017", "411044"]
 check_date = "05-05-2021"
 
 while 1:
@@ -57,25 +58,25 @@ while 1:
     for pins in pincodes:
 
         li_centers = getAvl(pins, check_date)
-        
+
         logger.info(li_centers)
 
         if not li_centers:
             logger.info('Server not returning any data.')
             pass
-            #data = {
+            # data = {
             #    'chat_id': '-599964487',
             #    'text': 'Server not returning any data.'
-            #}
+            # }
 
-            #requests.post('https://api.telegram.org/bot1797289547:AAE-8ENA0LyzCQZdPm0SXwDkTYEGTPZQruk/sendMessage',
+            # requests.post('https://api.telegram.org/bot1797289547:AAE-8ENA0LyzCQZdPm0SXwDkTYEGTPZQruk/sendMessage',
             #             data=data)
 
         else:
 
-            #for x in li_centers:
-                #logger.info(json.dumps(x, indent=4, sort_keys=False))
-                #print(json.dumps(x, indent=4, sort_keys=False))
+            # for x in li_centers:
+            # logger.info(json.dumps(x, indent=4, sort_keys=False))
+            # print(json.dumps(x, indent=4, sort_keys=False))
 
             for x in li_centers:
                 center_name = x["name"]
@@ -89,9 +90,13 @@ while 1:
                     if (avl > 0) and (age_limit == 18):
                         data = {
                             'chat_id': '-599964487',
-                            'text': 'Vaccine incoming.\n\n '+'Vaccine Capacity : '+str(avl)+'\n\n'+fee_type+' : Fee Type\n\n'+date+' : Date\n\n' + center_name + ' : Center Name.\n\n' + str(pincode) + ' : Pincode \n\n'+'Age Limit : ' + str(age_limit) + '\n\nLast Checked availability : ' + log_time
+                            'text': 'Vaccine incoming.\n\n ' + 'Vaccine Capacity : ' + str(
+                                avl) + '\n\n' + fee_type + ' : Fee Type\n\n' + date + ' : Date\n\n' + center_name + ' : Center Name.\n\n' + str(
+                                pincode) + ' : Pincode \n\n' + 'Age Limit : ' + str(
+                                age_limit) + '\n\nLast Checked availability : ' + log_time
                         }
 
-                        requests.post('https://api.telegram.org/bot1797289547:AAE-8ENA0LyzCQZdPm0SXwDkTYEGTPZQruk/sendMessage',
-                                     data=data)
+                        requests.post(
+                            'https://api.telegram.org/bot1797289547:AAE-8ENA0LyzCQZdPm0SXwDkTYEGTPZQruk/sendMessage',
+                            data=data)
     time.sleep(150)
